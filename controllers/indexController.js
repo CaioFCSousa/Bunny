2b1c62f1ac967b99419f931468a0907c3ea5135a
Caio Fernando <caiofernandocardoso6@gmail.com>
2024-04-11 13:36:31 -0300

    new

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,12 +1,16 @@
-// indexController.js
-
+const axios = require('axios');
 const Produto = require('../models/produtoModel'); // Importe o modelo do produto
 
 // Função para obter dados para a página inicial
 async function getIndexData(req, res) {
     try {
-        // Aqui você busca os produtos do banco de dados, supondo que você tenha um modelo "Produto"
-        const produtos = await Produto.find(); // Supondo que você esteja usando o Mongoose para interagir com o MongoDB
+        // Aqui você faz uma requisição GET para uma API externa
+        const response = await axios.get('https://api.example.com/produtos');
+        const produtos = response.data; // Supondo que a resposta da API seja um array de produtos
+
+        // Aqui você também pode buscar produtos do banco de dados, se necessário
+        // const produtos = await Produto.find(); // Supondo que você esteja usando o Mongoose para interagir com o MongoDB
+        
         res.render('index', { produtos }); // Passa os produtos para o template EJS
     } catch (error) {
         console.error(error);
