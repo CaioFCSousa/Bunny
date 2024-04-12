f8b70c43be51e6a21e51107a9bca6ac522c15371
Caio Fernando <caiofernandocardoso6@gmail.com>
2024-04-12 02:05:10 -0300

    new code

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://caiofernandocardoso6:<Acpsmc531@>@bunny-cluster.iy7o9mi.mongodb.net/?retryWrites=true&w=majority&appName=bunny-cluster";
+const uri = "mongodb+srv://caiofernandocardoso6:<Acpsmc531@>@bunny-cluster.iy7o9mi.mongodb.net/<bunny-cluster>?retryWrites=true&w=majority&appName=bunny-cluster";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
