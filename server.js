0477611845185d2799fb0d9134f32b4fc96910c4
Caio Fernando <caiofernandocardoso6@gmail.com>
2024-04-12 01:44:41 -0300

    new

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://caiofernandocardoso6:Acpsmc531@@bunny-cluster.iy7o9mi.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://caiofernandocardoso6:Acpsmc531@@bunny-cluster.iy7o9mi.mongodb.net/?retryWrites=true&w=majority&appName=bunny-cluster', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Conexão com MongoDB estabelecida'))
     .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
 
