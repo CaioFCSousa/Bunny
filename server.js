323856abe70b92f8abf6790605af6bae9a9f1263
Caio Fernando <caiofernandocardoso6@gmail.com>
2024-04-12 02:24:03 -0300

    new

--- server.js
+++ server.js
@@ -15,26 +15,30 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-const { MongoClient } = require("mongodb");
-const username = encodeURIComponent("<caiofernandocardoso6>");
-const password = encodeURIComponent("<Acpsmc531>");
-const cluster = "<bunny-cluster.iy7o9mi.mongodb.net>";
-const authSource = "<retryWrites=true>";
-const authMechanism = "<w=majority&appName=bunny-cluster>";
-let uri =
-  `mongodb+srv://${username}:${password}@${cluster}/?authSource=${authSource}&authMechanism=${authMechanism}`;
-const client = new MongoClient(uri);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<caiofernandocardoso6>:<password>@bunny-cluster.iy7o9mi.mongodb.net/?retryWrites=true&w=majority&appName=bunny-cluster";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const database = client.db("<dbName>");
-    const ratings = database.collection("<collName>");
-    const cursor = ratings.find();
-    await cursor.forEach(doc => console.dir(doc));
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
+    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+run().catch(console.dir);
+
 run().catch(console.dir);
 
 app.set('view engine', 'ejs');
