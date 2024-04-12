962b9c21a8d85f0df22080614046edfaf3b026fb
Caio Fernando <caiofernandocardoso6@gmail.com>
2024-04-12 03:29:58 -0300

    NEW

--- server.js
+++ server.js
@@ -15,26 +15,42 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const { MongoClient } = require("mongodb");
-const username = encodeURIComponent("<caiofernandocardoso6>");
-const password = encodeURIComponent("<BaGHeEtMlFePRs6B>");
-const cluster = "<cluster0>";
-const authSource = "<authSource>";
-const authMechanism = "<authMechanism>";
-let uri =
-  `mongodb+srv://${username}:${password}@${cluster}/?authSource=${authSource}&authMechanism=${authMechanism}`;
+
+// Configurações para conexão com o MongoDB
+const username = encodeURIComponent("caiofernandocardoso6");
+const password = encodeURIComponent("BaGHeEtMlFePRs6B");
+const cluster = "cluster0";
+const dbName = "seuBancoDeDados"; // Substitua pelo nome do seu banco de dados
+const collName = "suaColecao"; // Substitua pelo nome da sua coleção
+const authSource = "admin"; // Ou outro banco de dados de autenticação se aplicável
+const authMechanism = "SCRAM-SHA-1"; // Seu mecanismo de autenticação, geralmente SCRAM-SHA-1 ou SCRAM-SHA-256
+
+// URL de conexão com o MongoDB
+const uri = `mongodb+srv://${username}:${password}@${cluster}.iy7o9mi.mongodb.net/${dbName}?authSource=${authSource}&authMechanism=${authMechanism}`;
+
+// Cliente MongoDB
 const client = new MongoClient(uri);
+
 async function run() {
   try {
     await client.connect();
-    const database = client.db("<dbName>");
-    const ratings = database.collection("<collName>");
-    const cursor = ratings.find();
+    console.log("Conexão com MongoDB estabelecida");
+
+    const database = client.db(dbName);
+    const collection = database.collection(collName);
+
+    // Exemplo: buscar todos os documentos na coleção
+    const cursor = collection.find();
     await cursor.forEach(doc => console.dir(doc));
+
   } finally {
     await client.close();
   }
 }
+
 run().catch(console.dir);
+
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
